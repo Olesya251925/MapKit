800292e3d4d96c3c72221c97a76a120abd1f3e84
olesyakirillova <olesya.kiril2014@gmail.com>
2024-04-27 23:24:10 +0700

    а

--- app/src/main/java/com/example/mapkit/MainActivity.kt
+++ app/src/main/java/com/example/mapkit/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.mapkit
 
+import android.app.blob.BlobStoreManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.map.CameraPosition
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mapView: com.yandex.mapkit.mapview.MapView
+
+    // Глобальная переменная для хранения объекта сессии поиска
+    private var searchSession: BlobStoreManager.Session? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +30,21 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        // Находим плавающую кнопку и добавляем обработчик нажатия
+        val fabLocation = findViewById<FloatingActionButton>(R.id.fab_location)
+        fabLocation.setOnClickListener {
+            getKemerovoCoordinates()
+        }
+    }
+
+
+    private fun getKemerovoCoordinates() {
+        val kemerovoCoordinates = Point(55.355202, 86.086841)
+
+        // Перемещаем камеру на координаты города Кемерово
+        mapView.map.move(
+            CameraPosition(kemerovoCoordinates, 11.0f, 0.0f, 0.0f)
+        )
     }
     override fun onStart() {
         super.onStart()
@@ -35,4 +57,5 @@ class MainActivity : AppCompatActivity() {
         MapKitFactory.getInstance().onStop()
         super.onStop()
     }
+
 }
